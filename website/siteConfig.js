a14692650d32988be800801467e85b5d399cdee6
rohan rath <jaya@yellowant.com>
2019-04-23 15:41:00 +0530

    search

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -29,7 +29,11 @@ const siteConfig = {
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
-
+  //Doc search
+  algolia: {
+    apiKey: '00808d264b6611cda3ba4754f7005d69',
+    indexName: 'zenduty',
+  },
   // Used for publishing and more
   projectName: 'docs',
   organizationName: 'zenduty',
@@ -39,7 +43,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'Introduction'}
+    {doc: 'Introduction'},
+    {search: true}
   ],
 
   // If you have users set above, you add it here:
